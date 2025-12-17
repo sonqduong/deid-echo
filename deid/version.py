3fd318672453b46c9f1fefc30bc225785d5bb0f0
Son Duong-MSH <son.duong@mssm.edu>
2025-12-17 14:03:33 -0500

    more version

--- deid/version.py
+++ deid/version.py
@@ -14,6 +14,8 @@ LICENSE = "LICENSE"
 INSTALL_REQUIRES = (
     "matplotlib",
     "numpy>=1.20",
-    "pydicom>=3.0.0,<4.0.0",
+    "pydicom=3.0.1",
     "python-dateutil",
+    "pandas",
+    "gdcm",
 )
